e97749e545ff59a007030b0c5ef3b180dbaff33a
XLDou <douxlghost@gmail.com>
2019-10-02 10:14:42 -0500

    add zshrc

--- build_link.sh
+++ build_link.sh
@@ -3,5 +3,9 @@ ln -sf ~/.dotfiles/.vim ~/.vim
 ln -sf ~/.dotfiles/.emacs ~/.emacs
 ln -sf ~/.dotfiles/.emacs.d/ ~/.emacs.d
 ln -sf ~/.dotfiles/.ctags ~/.ctags
+
+mkdir -p ~/.vim/syntax/
+touch -a ~/.vim/syntax/c.vim
 ln -sf ~/.dotfiles/.vim/syntax/c.vim ~/.vim/syntax/c.vim
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 
